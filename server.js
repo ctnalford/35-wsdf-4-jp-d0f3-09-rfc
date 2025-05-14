7c847db4e8b9801515ec693754c336f3be4fda27
ctnalford <139728680+ctnalford@users.noreply.github.com>
2025-05-14 01:11:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Optional: fallback for any route (e.g., for SPAs)
 // Adjust this path if your "main" index.html is nested
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'x30-sgc-a40dsf-lk-fd/13r0f-fgfg-0xe3-rfgh/FJSDGF-F-WFSGE-E4-VFGGFHG-FDSER354W2-TGFCDAR4-W4TRGVC/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'x30-sgc-a40dsf-lk-fd/13r0f-fgfg-0xe3-rfgh/FJSDGF-F-WFSGE-E4-VFGGFHG-FDSER354W2-TGFCDAR4-W4TRGVC/zaq/index.html'));
 });
 
 app.listen(port, () => {
